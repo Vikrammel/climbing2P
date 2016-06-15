62e04bf8db51e5864a2e56b2f5e1cb1375e5b758
Colin Maher <colinmaher@users.noreply.github.com>
2016-06-15 02:49:41 -0700

    updated engine.js

--- js/engine.js
+++ js/engine.js
@@ -28,8 +28,9 @@ var Engine = (function(global){
 
         canvas.width = newWidth;
         canvas.height = newHeight;
+        //render();
     };
-    
+
 	resizeGame();
 	window.addEventListener('resize', resizeGame, false);
 
@@ -46,7 +47,7 @@ var Engine = (function(global){
         /* Call our update/render functions, pass along the time delta to
          * our update function since it may be used for smooth animation.
          */
-        update(dt);
+        //update(dt);
         render();
 
         /* Set our lastTime variable which is used to determine the time delta
@@ -88,10 +89,8 @@ var Engine = (function(global){
      * render methods.
      */
     function updateEntities(dt) {
-        allLedges.forEach(function(ledge) {
-            ledge.update(dt);
-        });
-        //player.update();
+        game.update();
+        player.update();
     }
         /* This function initially draws the "game level", it will then call
      * the renderEntities function. Remember, this function is called every
@@ -103,37 +102,8 @@ var Engine = (function(global){
         /* This array holds the relative URL to the image used
          * for that particular row of the game level.
          */
-        var rowImages = [
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-                'images/stone-block.png',   // Row 1 of 3 of stone
-                'images/stone-block.png',   // Row 2 of 3 of stone
-                'images/stone-block.png',   // Row 3 of 3 of stone
-            ],
-            numRows = 6,
-            numCols = 5,
-            row, col;
-
-        /* Loop through the number of rows and columns we've defined above
-         * and, using the rowImages array, draw the correct image for that
-         * portion of the "grid"
-         */
-        for (row = 0; row < numRows; row++) {
-            for (col = 0; col < numCols; col++) {
-                /* The drawImage function of the canvas' context element
-                 * requires 3 parameters: the image to draw, the x coordinate
-                 * to start drawing and the y coordinate to start drawing.
-                 * We're using our Resources helpers to refer to our images
-                 * so that we get the benefits of caching these images, since
-                 * we're using them over and over.
-                 */
-                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
-            }
-        }
-
-
         renderEntities();
+
     }
 
     /* This function is called by the render function and is called on each game
@@ -144,9 +114,7 @@ var Engine = (function(global){
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
-        allLedges.forEach(function(ledge) {
-            ledge.render();
-        });
+        game.render();
 
         player.render();
     }
